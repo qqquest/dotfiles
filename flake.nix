93ae726fe0a2f49f8160561dedafaf583bb28845
qqquest <159469868+qqquest@users.noreply.github.com>
2024-04-29 02:51:37 -0300

    remove haumea from flake inputs

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-
-    haumea = {
-      url = "github:nix-community/haumea/v0.2.2";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, ... } @inputs:{
